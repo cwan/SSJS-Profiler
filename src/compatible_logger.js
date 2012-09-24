b96db810001d43a3bc3b869f60cac7dd0144f304
cwan <cwan.oss@gmail.com>
2012-09-25 01:23:19 +0900

    #5 V6でsystem.logに出力するようする。ライセンスコメントも修正。

--- src/compatible_logger.js
+++ src/compatible_logger.js
@@ -1,18 +1,15 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 
 function init() {
@@ -22,7 +19,7 @@ function init() {
 			get : function get(name) {
 			
 				if (typeof Logger === "undefined") {
-					return new CompatibleLogger(name, Debug.print);
+					return new CompatibleLogger(name, echo);
 				} else {
 					return (typeof name !== "undefined") ? Logger.getLogger(name) : Logger.getLogger();
 				}
